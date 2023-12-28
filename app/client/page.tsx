05624701d541975b5ddb72bf0ffd7ac3ab967b96
suleimankrim <sulimankareem90@gmail.com>
2023-12-28 09:29:23 -0800

    newD

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,13 +1,11 @@
 import data from "@/data/customer.json";
 import Image from "next/image";
 import Card from "./components/Card";
-import RequestFree from "../components/first/RequestFree";
 
 export default function page() {
   return (
     <div className="h-screen w-screen bg-whileWhite overflow-x-hidden">
       <Card></Card>
-      <RequestFree text={"join us as content creater"} />
     </div>
   );
 }
